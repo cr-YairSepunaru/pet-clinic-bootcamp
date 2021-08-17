eb3e45204a719dbbec3c26d8377fca0cdb01fe58
Yair Sepunaru <yair.sepunaru@cybereason.com>
2021-08-17 10:26:44 +0300

    Initial commit

---
+++ src/main/java/com/example/petclinicbootcamp/PetClinicBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.example.petclinicbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicBootcampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicBootcampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petclinicbootcamp/PetClinicBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petclinicbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicBootcampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
